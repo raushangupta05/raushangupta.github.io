5f47b28dbb683de804f64f41b2e97e37d4f2da60
Raushan Kumar <ravisoni62058@gmail.com>
2025-01-16 18:00:46 +0530

    Javascript Page of portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic JavaScript for interactivity (if needed)
+document.addEventListener('DOMContentLoaded', function() {
+    // You can add any JavaScript functionality here if needed
+    console.log('Portfolio website loaded!');
+});
